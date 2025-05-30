1b7073731894481afac16d6b6bea46e95d8119de
malachitech <tmezeh9@gmail.com>
2025-05-30 15:22:36 +0100

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-      return [
-        {
-          source: '/:path*.txt',
-          destination: '/:path*',
-        },
-      ];
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
